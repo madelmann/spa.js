ce94ba06e8039aba3e187f551555b2811f168587
Michael Adelmann <noreply@github.com>
2024-07-15 00:23:46 +0200

    improved examples in loadingView

--- plugins/loadingView/plugin.js
+++ plugins/loadingView/plugin.js
@@ -12,7 +12,7 @@ mLoadingPlugin = {
         mElProgress = $( "#progress" );
 
         progress = 0;
-        progressIncrease = 100 / this.WaitForTicks;
+        progressIncrease = 50 / this.WaitForTicks;      // first half of progress
     },
 
     OnLoadReady: function() {
@@ -30,16 +30,22 @@ mLoadingPlugin = {
     OnTick: function() {
         progress = parseFloat( mElProgress.ariaValueNow );
 
+        // process base data
         progress += progressIncrease;   // see code below if you want to split loading into multiple stages
 
-        // // base config
-        // if ( progress < 50 ) {
+        // // process base data
+        // if ( progress < 50 - progressIncrease ) {
         //     progress += progressIncrease;
         // }
-        // // load extended data
-        // else if ( progress == 50 ) {
+        // // fetch extended data
+        // else if ( progress < 51 ) {
+        //     progress = 50;
+
+        //     progressIncrease = 50 / numberOfTicksForSecondHalfOfProgress;     // second half of progress
+
         //     // trigger loading extended data
         // }
+        // // process extended data
         // else {
         //     progress += progressIncrease;
         // }
