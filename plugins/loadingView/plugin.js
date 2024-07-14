52661512d784cbab858ff03a75979a049fd7d429
Michael Adelmann <noreply@github.com>
2024-07-15 00:38:04 +0200

    improved example in loadingView

--- plugins/loadingView/plugin.js
+++ plugins/loadingView/plugin.js
@@ -31,24 +31,24 @@ mLoadingPlugin = {
         progress = parseFloat( mElProgress.ariaValueNow );
 
         // process base data
-        progress += progressIncrease;   // see code below if you want to split loading into multiple stages
+        // progress += progressIncrease;   // see code below if you want to split loading into multiple stages
 
-        // // process base data
-        // if ( progress < 50 - progressIncrease ) {
-        //     progress += progressIncrease;
-        // }
-        // // fetch extended data
-        // else if ( progress < 51 ) {
-        //     progress = 50;
+        // process base data
+        if ( progress < 50 - progressIncrease ) {
+            progress += progressIncrease;
+        }
+        // fetch extended data
+        else if ( progress < 51 ) {
+            progressIncrease = 50 / 1;     // second half of progress
 
-        //     progressIncrease = 50 / numberOfTicksForSecondHalfOfProgress;     // second half of progress
+            progress = 50 + progressIncrease;
 
-        //     // trigger loading extended data
-        // }
-        // // process extended data
-        // else {
-        //     progress += progressIncrease;
-        // }
+            // trigger loading extended data
+        }
+        // process extended data
+        else {
+            progress += progressIncrease;
+        }
 
         mElProgress.ariaValueNow = progress;
         mElProgress.style.width  = progress + "%";
